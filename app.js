88025ccc7f35ea80dd90778b9abc24f648459c8b
Carson Christian <cscade@gmail.com>
2012-06-11 19:09:17 -0400

    switch to express

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 // 
-//  app.js
-//  seeker-brewing
-//  
-//  Created by Carson Christian on 2012-06-11.
-//  Copyright 2012 Carson Christian. All rights reserved.
+//	app.js
+//	seeker-brewing
+//	
+//	Created by Carson Christian on 2012-06-11.
+//	Copyright 2012 Carson Christian. All rights reserved.
 // 
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	app = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+	app.set('views', __dirname + '/jade');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public', {
+		maxAge: 86400000 // 1 Day
+	}));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+	app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', function (req, res) {
+	res.render('dashboard.jade', { layout: false });
+});
+
+app.get('/upload', function (req, res) {
+	res.render('upload.jade', { layout: false });
+});
+
+app.post('/upload', function (req, res) {
+	res.end(JSON.stringify({ files: req.files, body: req.body }));
+});
+
+app.listen(80, function(){
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
