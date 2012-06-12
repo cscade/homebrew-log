4bd683e1f4bfa966b3b21f8a5e44af5398361a77
Carson Christian <cscade@gmail.com>
2012-06-11 20:07:56 -0400

    hack together uploading with memory

--- app.js
+++ app.js
@@ -11,7 +11,12 @@
  */
 
 var express = require('express'),
+	xml2js = require('xml2js'),
+	fs = require('fs'),
+	connect = require('connect'),
 	app = express.createServer();
+	
+var recipes = [];
 
 // Configuration
 
@@ -36,16 +41,63 @@ app.configure('production', function () {
 
 // Routes
 app.get('/', function (req, res) {
-	res.render('dashboard.jade', { layout: false });
+	res.render('dashboard.jade', {
+		layout: false,
+		locals: {
+			/*
+				TODO real recipes
+			*/
+			recipes: recipes
+		}
+	});
 });
 
 app.get('/upload', function (req, res) {
-	res.render('upload.jade', { layout: false });
+	render.upload.call(res);
 });
 
 app.post('/upload', function (req, res) {
-	res.end(JSON.stringify({ files: req.files, body: req.body }));
+	var parser = new xml2js.Parser();
+	
+	if (req.files && req.files.recipe) {
+		if (req.files.recipe.size < 1) return render.upload.call(res, {
+				message: '\
+					<div class="alert">\
+						<strong>Uh...</strong> Forget something?\
+					</div>'
+			});
+		fs.readFile(req.files.recipe.path, function(e, data) {
+			if (e) return res.end(JSON.stringify(e));
+			parser.parseString(data, function (e, result) {
+				if (e || !result.RECIPE) return render.upload.call(res, {
+						message: '\
+							<div class="alert alert-error">\
+								<strong>Nope.</strong> Upload BeerXML v1 only.\
+							</div>'
+					});
+				recipes.push({
+					name: result.RECIPE.NAME,
+					modified: new Date(),
+					data: result.RECIPE
+				});
+				res.redirect('/');
+			});
+		});
+	}
+	else render.upload.call(res);
 });
+
+// renderers
+var render = {
+	upload: function (data) {
+		this.render('upload.jade', {
+			layout: false,
+			locals: connect.utils.merge({
+				message: '',
+			}, data || {})
+		});
+	}
+};
 
 app.listen(80, function(){
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
