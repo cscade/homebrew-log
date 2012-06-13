26ce2f13be66370ee15959072eb30d6c00d21541
Carson Christian <cscade@gmail.com>
2012-06-13 10:18:09 -0400

    add padding option to rounder

--- modules/lib/convert.js
+++ modules/lib/convert.js
@@ -18,7 +18,24 @@ exports.kilograms = {
 	}
 };
 
-exports.round = function (precision) {
+/*
+round
+
+Round a number to the given precision.
+
+ * @param {Number} precision Places to round to.
+ * @param {Bool} pad Pad zeros after the decimal place, if any.
+*/
+exports.round = function (precision, pad) {
+	var result, requested = precision;
+	
 	precision = Math.pow(10, precision || 0).toFixed(precision < 0 ? -precision : 0);
-	return Math.round(this * precision) / precision;
+	result = Math.round(this * precision) / precision;
+	if (pad && result.toString().indexOf('.') !== -1) {
+		result = result.toString();
+		while ((result.length - result.indexOf('.')) <= requested) {
+			result = result + '0';
+		}
+	}
+	return result;
 };
