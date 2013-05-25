185466574d3a463823c0e109a552b718faba2f41
Carson Christian <cc@amplego.com>
2013-05-25 10:20:05 -0400

    remove tweak, use convey, modernize startup sequence

--- app.js
+++ app.js
@@ -10,13 +10,15 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-	winston = require('winston'),
-	http = require('http'),
-	fs = require('fs'),
-	path = require('path'),
-	app = express(),
-	tweak = require('./lib/tweak');
+var Convey = require('convey').Convey;
+
+var app;
+var async = require('async');
+var express = require('express');
+var http = require('http');
+var winston = require('winston');
+
+app = express();
 
 // Configuration
 app.log = new (winston.Logger)({
@@ -32,8 +34,8 @@ app.log = new (winston.Logger)({
 app.configure(function(){
 	app.set('views', __dirname + '/jade');
 	app.set('view engine', 'jade');
-	app.set('config', JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'config.json'), 'utf-8'))[app.get('env')]);
-	app.set('version', JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8')).version);
+	app.set('config', require('./config/config.json')[app.get('env')]);
+	app.set('version', require('./package.json').version);
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
@@ -59,15 +61,62 @@ Number.from = function (item) {
 // couch
 require('./lib/cradle').initialize(app);
 
-// tweak
-tweak.check(app);
-
-// bcs-controllers
-require('./lib/controllers').initialize(app);
-
 // routes
 require('./modules/routes')(app);
 
-http.createServer(app).listen(app.get('config').listen, function () {
-	app.log.info(app.get('config').name + ' ' + app.get('version') + ' listening on port ' + app.get('config').listen);
+/*
+	Convey database check.
+*/
+var convey = new Convey({
+	extendDocument: {
+		resource: 'convey-document'
+	}
+});
+convey
+	.on('start', function (info) {
+		app.log.info('[Convey] start: ' + info.couch + ' for application version ' + info.version);
+	})
+	.on('database:start', function (info) {
+		app.log.info('[Convey] database start: ' + info.database);
+	})
+	.on('resource:fresh', function (info) {
+		app.log.info('[Convey] resource already fresh: ' + info.resource);
+	})
+	.on('resource:stale', function (info) {
+		app.log.info('[Convey] resource stale: ' + info.resource + (info.forced ? ' (forced)' : ''));
+	})
+	.on('target:done', function (info) {
+		app.log.info('[Convey] target done: ' + info.database + ', created: ' + info.created + ', updated: ' + info.updated);
+	})
+	.on('resource:done', function (info) {
+		app.log.info('[Convey] resource done: ', info.resource);
+	})
+	.on('database:done', function (info) {
+		app.log.info('[Convey] database done: ' + info.database);
+	})
+	.on('done', function (info) {
+		app.log.info('[Convey] done: ' + info.duration + 's elapsed');
+	});
+
+/*
+	Startup.
+*/
+async.series({
+	convey: function (next) {
+		convey.check(app.get('config').couch.url + ':' + app.get('config').couch.port, app.get('version'), 'config/convey.json', app.get('env') !== 'production', next);
+	},
+	bcs: function (next) {
+		require('./lib/controllers').initialize(app, next);
+	},
+	listen: function (next) {
+		var server;
+		
+		server = http.createServer(app);
+		server.on('error', next);
+		server.on('listening', next);
+		server.listen(app.get('config').listen);
+	}
+}, function (e) {
+	if (e) return app.log.error('[Error]', e);
+	app.log.info('[Application] ' + app.get('config').name + ' ' + app.get('version') + ' listening on port ' + app.get('config').listen);
 });
